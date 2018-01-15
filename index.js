bed674161d99aad1ee3d9539d2bf23cd33298962
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-15 17:58:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 
 function exactMatch(drivers, attribute) {
   return drivers.filter(function (driver) {
-    console.log(driver[Object.keys(attribute)] === Object.values(attribute));
+    console.log(driver[Object.keys(attribute)]);
   })
 }
 

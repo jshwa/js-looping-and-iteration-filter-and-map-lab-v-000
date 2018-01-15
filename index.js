7c1e5b45fa2fa58f8454e2d09231b6c84cf47906
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-15 17:44:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // Code your solution here:
-function driversWithRevenueOver() {
-  
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function (driver) {
+    console.log(driver)
+  })
 }
 
 function driverNamesWithRevenueOver() {
-  
+
 }
 
 function exactMatch() {
-  
+
 }
 
 function exactMatchToList() {
-  
+
 }

1bc0cb1961d3be726e53b00b29049661c89bd35f
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-15 17:47:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (driver) {
-    return driver.revenue > revenue
+    return driver.revenue > revenue;
   })
 }
 
 function driverNamesWithRevenueOver(drivers, revenue) {
-  return console.log(driversWithRevenueOver)
+  return console.log(driversWithRevenueOver(drivers, revenues))
 }
 
 function exactMatch() {
